3bbd12a39a2576b8680e7c11fad6e48c05f71c59
Emiliano C <emiliano.cereceres@student.cart.org>
2023-01-21 23:11:18 -0800

    light glow
    glow effect using border and background color for light bulb. Removed commented code.

---
+++ scripts/javascript.js
@@ -0,0 +1,10 @@
+const bulb = document.getElementById("bulb");
+const lightArea = document.getElementById("glow");
+const garageLight = document.getElementById("garage-light");
+const toppy = document.getElementById("tippy");
+
+console.log(toppy.style);
+
+function glowHover() {
+    toppy.style.backgroundColor = "Red";
+};

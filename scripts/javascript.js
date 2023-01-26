696a5f59d0f68f552b1a09516666f5e42f01b806
Emiliano C <emiliano.cereceres@student.cart.org>
2023-01-25 17:55:02 -0800

    lightbulb functions
    javascript functions to turn on light bulb in garage

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,10 +1,25 @@
 const bulb = document.getElementById("bulb");
 const lightArea = document.getElementById("glow");
 const garageLight = document.getElementById("garage-light");
+const arrow = document.getElementById("arrow");
 const toppy = document.getElementById("tippy");
 
 console.log(toppy.style);
 
 function glowHover() {
-    toppy.style.backgroundColor = "Red";
+    lightArea.style.backgroundColor = "#feffb750";
+    bulb.style.boxShadow = "#feffb7 0 0 50px 15px";
 };
+
+function glowOff() {
+    lightArea.style.backgroundColor = "#00000000";
+    bulb.style.boxShadow = "none";
+};
+
+function lightOn() {
+    lightArea.style.backgroundColor = "#feffb725";
+    bulb.style.boxShadow = "#feffb728 0 0 150px 115px";
+    bulb.style.opacity = 1;
+    garageLight.style.backgroundColor = "#00000000";
+    arrow.style.display = "block";
+}
